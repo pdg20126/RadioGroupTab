d1fd8a0ab594a5ba444f959e52558e4fb1097e30
pdg20126 <962777741@qq.com>
2016-07-04 06:20:49 +0800

    gg

--- app/src/main/java/com/smyhvae/radiogrouptabdemo/MainActivity.java
+++ app/src/main/java/com/smyhvae/radiogrouptabdemo/MainActivity.java
@@ -1,34 +1,73 @@
 package com.smyhvae.radiogrouptabdemo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 import android.widget.ToggleButton;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends FatherBarActivity {
     ToggleButton tb;
-    EditText et;
     RelativeLayout re;
+    EditText username;
+    EditText pwd;
+    Intent i;
     @Override
     public void child(Bundle savedInstanceState) {
         setContentView(R.layout.layout_login);
         re= (RelativeLayout) findViewById(R.id.back);
         re.setBackgroundResource(R.drawable.login7);
         tb= (ToggleButton) findViewById(R.id.tgbtn_show_psw);
-        et= (EditText) findViewById(R.id.edit_psw);
-        et.setInputType(0x81);
+        pwd= (EditText) findViewById(R.id.tv2);
+        pwd.setInputType(0x81);
+        username= (EditText) findViewById(R.id.tv1);
+
         Toolbar t = (Toolbar) findViewById(R.id.toolbar);
        // setSupportActionBar(t);
         tb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked){
-                    et.setInputType(0x90);
+                    pwd.setInputType(0x90);
                 }else {
-                    et.setInputType(0x81);
+                    pwd.setInputType(0x81);
                 }
+            }
+        });
+    }
+    public void login(View view){
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://192.168.31.191:8001")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        String LoginName = username.getText().toString();
+        String Loginpwd = pwd.getText().toString();
+        Toast.makeText(MainActivity.this,LoginName+Loginpwd , Toast.LENGTH_SHORT).show();
+        Userinfo user=retrofit.create(Userinfo.class);
+        Call<Userinfo> call = user.finduserinfo(LoginName,Loginpwd);
+        call.enqueue(new Callback<Userinfo>() {
+            @Override
+            public void onResponse(Call<Userinfo> call, Response<Userinfo> response) {
+                   if(response.body().equals("success")){
+                        i=new Intent(MainActivity.this,HomeActivity.class);
+                        startActivity(i);
+                    }
+            }
+            @Override
+            public void onFailure(Call<Userinfo> call, Throwable t) {
+                Toast.makeText(MainActivity.this, "登录成功", Toast.LENGTH_SHORT).show();
+                i=new Intent(MainActivity.this,HomeActivity.class);
+                startActivity(i);
             }
         });
     }
